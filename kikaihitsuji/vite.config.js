b80b651bf7120928d0cac98ade2170426b039780
automuttonproject <automuttonproject@gmail.com>
2026-03-21 22:51:44 +0900

    Update vite.config.js

--- kikaihitsuji/vite.config.js
+++ kikaihitsuji/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './automuttonproject.github.io/',
+  base: './automuttonproject.github.io/kikaihitsuji/',
 })
